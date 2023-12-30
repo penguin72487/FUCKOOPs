3e5ad7057a121184264705ed09ad1c2322e793de
penguin_72487 <agar.io6100@gmail.com>
2023-12-30 11:45:58 +0800

    ui merge

--- ui.cpp
+++ ui.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+
 class Button {
 public:
     sf::RectangleShape shape;
@@ -55,7 +56,6 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-                window.close();
 
             // check if buttons are clicked
             if (illustrateButton.isClicked(event)) {
@@ -89,14 +89,6 @@ int main() {
         window.draw(developerButton.shape);
         window.draw(developerButton.text);
         window.display();
-}
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
->>>>>>> 6823b258505da60362886350c25fe54ea50f6167
     }
 
     return 0;
