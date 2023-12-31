2a4bdc9af4c93fe62bd60c03e62563f1bd1e5fdd
penguin72487 <agar.io6100@gmail.com>
2023-12-31 14:05:01 +0800

    UI Manager

---
+++ UI/uiManerger.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "UIManager.hpp"
+
+
+int main() {
+    UIManager uiManager;
+
+    uiManager.renderScreen(0);
+
+    return 0;
+}
