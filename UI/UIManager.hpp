cdb1848e465ece74d0bfc6538a49edcdfc9da96f
yieie <ison632413672@gmail.com>
2024-01-01 22:39:29 +0800

    add gameselectpic

--- UI/UIManager.hpp
+++ UI/UIManager.hpp
@@ -222,6 +222,10 @@ private:
     Button MenuButton;
     Button BasicButton;
     Button AdvanceButton;
+    sf::Texture basicpic;
+    sf::Sprite basicpicture;
+    sf::Texture ultimatepic;
+    sf::Sprite ultimatepicture;
 
 public:
     GameSelectionMenu(sf::RenderWindow& window) : UIComponent(window),
@@ -235,7 +239,20 @@ public:
         advancetext=sf::Text("ADVANCE\n9x9", font, 65);
         advancetext.setFillColor(sf::Color(0, 0, 0));
         advancetext.setPosition(722, 683);
+
+        if (!basicpic.loadFromFile("../data/png/ui/basic.png")) {
+            std::cout << "Texture load failed\n";
+        }
+        basicpicture.setTexture(basicpic);
+        basicpicture.setScale(0.15, 0.15);
+        basicpicture.setPosition(375, 190);
         
+        if (!ultimatepic.loadFromFile("../data/png/ui/ultimate.png")) {
+            std::cout << "Texture load failed\n";
+        }
+        ultimatepicture.setTexture(ultimatepic);
+        ultimatepicture.setScale(0.3, 0.3);
+        ultimatepicture.setPosition(375, 620);
     }
     Screen render() override {
         std::cout << "Game Selection Menu: [List of Games]" << std::endl;
@@ -251,6 +268,8 @@ public:
             window.draw(MenuButton.text);
             window.draw(basictext);
             window.draw(advancetext);
+            window.draw(basicpicture);
+            window.draw(ultimatepicture);
 
             window.display();
             sf::Event event;
