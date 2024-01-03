08961a041db5bdf9c9089f70c1177c8e6f77f61c
penguin72487 <agar.io6100@gmail.com>
2024-01-03 20:09:47 +0800

    Refactor win condition checks in Ultimate game

--- Game/Ultimate.hpp
+++ Game/Ultimate.hpp
@@ -134,38 +134,30 @@ class Ultimate : public Game{
     // }
     player check_Win() override {
         // 檢查每一行
-        for (int i = 0; i < 3; ++i) {
-            if (board[i][0] != player::none && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
-                return board[i][0];
-            }
+        for (int i = 0; i < 3; i++) {
+            if (isLineWin(board[i][0], board[i][1], board[i][2])) return board[i][0];
         }
 
         // 檢查每一列
-        for (int i = 0; i < 3; ++i) {
-            if (board[0][i] != player::none && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-                return board[0][i];
-            }
+        for (int i = 0; i < 3; i++) {
+            if (isLineWin(board[0][i], board[1][i], board[2][i])) return board[0][i];
         }
 
         // 檢查對角線
-        if (board[0][0] != player::none && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
-            return board[0][0];
-        }
-        if (board[0][2] != player::none && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
-            return board[0][2];
-        }
+        if (isLineWin(board[0][0], board[1][1], board[2][2])) return board[0][0];
+        if (isLineWin(board[0][2], board[1][1], board[2][0])) return board[0][2];
 
-        // 檢查是否還有空格，如果有，則遊戲還未結束
-        for (auto &i : board) {
-            for (auto &j : i) {
-                if (j == player::none) {
-                    return player::none;
-                }
+        // 檢查平手
+        for (auto &row : board) {
+            for (auto cell : row) {
+                if (cell == player::none) return player::none;
             }
         }
 
-        // 如果棋盤已滿且沒有玩家獲勝，則為平局
         return player::draw;
+    }
+    bool isLineWin(player c1, player c2, player c3) {
+        return c1 != player::none && c1 == c2 && c2 == c3;
     }
     UIComponent::Screen getGameMode() override{
         return UIComponent::Screen::GAME_ULTIMATE_INTERFACE;
