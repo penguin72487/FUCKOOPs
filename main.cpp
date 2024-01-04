121ae2863ce0f21ec797adae3dd9d8befbb49392
penguin72487 <agar.io6100@gmail.com>
2024-01-04 18:28:28 +0800

    Remove logging code from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "UI/UIManager.hpp"
-#include <iostream>
-#include <fstream>
 int main() {
-    std::ofstream logFile("log.txt");
-    std::cerr.rdbuf(logFile.rdbuf()); // 重定向 std::cerr 到 log.txt 文件
-    std::cout.rdbuf(logFile.rdbuf()); // 重定向 std::cout 到 log.txt 文件
     UIManager uiManager;
     uiManager.run();
-    logFile.close(); // 別忘了在程序結束時關閉文件
     return 0;
 }
